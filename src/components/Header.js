3de8439763acbb84bffe12f8f0d3f6e4b7010d03
Carlos Bastida <bastidacarlos996@gmail.com>
2026-07-12 18:21:10 -0600

    Añadiendo logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import { ReactComponent as Logo} from '../assets/nysl_logo.svg'
+import { Link } from 'react-router-dom';
 import styles from '../styles/Header.module.css'
 
 const Header = () => (
     <header className={styles.headerContainer}>
-        <Logo className={styles.logoCustom}/>
-        <span className={styles.titleContainer}>NYSL</span>
+        <Link to={'/'} className={styles.linkHome}> 
+            <Logo className={styles.logoCustom}/>
+            <span className={styles.titleContainer}>NYSL</span>
+        </Link>
     </header>
 ) 
 
